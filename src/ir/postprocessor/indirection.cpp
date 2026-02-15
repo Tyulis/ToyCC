0cf3a67f34f9a5554a4a2cdc50e107b76920b1b3
Grégori MIGNEROT <gregori.mignerot@laposte.net>
2026-02-15 14:42:52 +0100

    Fix indirection splitting off-by-one

--- src/ir/postprocessor/indirection.cpp
+++ src/ir/postprocessor/indirection.cpp
@@ -28,29 +28,20 @@ namespace toycc::ir {
         Operand pointer = operand;
         std::shared_ptr<Type> pointer_type = pointer.base_type();
         size_t top_level = 0;
-        for (size_t level = 0; level < operand.indices.size(); level++) {
+        for (size_t level = 0; level < operand.indices.size() - 1; level++) {
             const Operand& index = operand.indices[level];
+            std::shared_ptr<Type> referenced_type = pointer_type->dereference(index.as_index(), index.location);
 
-            switch (pointer_type->category) {
-                case TypeCategory::ARRAY:
-                case TypeCategory::STRUCT:
-                case TypeCategory::UNION:
-                    break;  // No indirection at this level, indices are still from the same pointer
-
-                case TypeCategory::POINTER: {
-                    std::shared_ptr<Type> referenced_type = pointer_type->dereference(index.as_index(), index.location);
-                    std::shared_ptr<Declaration> pointee = declare_temporary(scope, referenced_type, index.location);
-                    const Operand reference = Operand {operand.value, operand.location, {operand.indices.begin() + top_level, operand.indices.begin() + level}, referenced_type};
-                    scope->add_statement(Statement::make_unary_operation(operand.location, StatementTag::COPY, reference, pointee));
-                    pointer = Operand {pointee, operand.location, {operand.indices.begin() + level + 1, operand.indices.end()}};
-                    top_level = level + 1;
-                    break;
-                }
-
-                default: throw Diagnostic(DiagnosticLevel::ERROR, std::format("Type `{}` can't be dereferenced", pointer_type->ir_code()), index.location);
+            if (referenced_type->category == TypeCategory::POINTER) {
+                std::shared_ptr<Declaration> pointee = declare_temporary(scope, referenced_type, index.location);
+                const Operand reference = Operand {pointer.value, pointer.location, {operand.indices.begin() + top_level, operand.indices.begin() + level + 1}};
+                scope->add_statement(Statement::make_unary_operation(operand.location, StatementTag::COPY, reference, pointee));
+                pointer = Operand {pointee, operand.location, {operand.indices.begin() + level + 1, operand.indices.end()}};
+                top_level = level + 1;
+                break;
             }
 
-            pointer_type = pointer_type->dereference(index.as_index(), index.location);
+            pointer_type = referenced_type;
         }
 
         return pointer;
