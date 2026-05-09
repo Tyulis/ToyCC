ac30cbb9d815a259847c864468b0d372cf73f146
Grégori MIGNEROT <gregori.mignerot@laposte.net>
2026-05-09 17:27:00 +0200

    Disable glibc extensions for now
    - ToyCC is now able to parse the glibc stdlib.h

--- include/__toycc_prologue.h
+++ include/__toycc_prologue.h
@@ -9,4 +9,7 @@
 #define __STDC_NO_THREADS__ 1
 #define __STDC_NO_VLA__ 1
 
+// At least for now, stick to standard C23 and disable glibc extensions
+#define _ISOC23_SOURCE 1
+
 #endif /* __TOYCC_PROLOGUE_H__ */
